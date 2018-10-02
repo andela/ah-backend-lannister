3c2b8c2988aafe675873f41eb4b6476f2175eadf
Amos Welike <amoswelike@Davids-MacBook-Pro-3.local>
2018-10-02 17:13:45 +0300

    ft(read_time):add functionality to show read time of an article
    - add functionality to show read time of an article
    
    [DELIVERS #160617673]
    
    ft(upgrade):add articles  app to authors project for creating articles
    
    - create articles app
    [DELIVERS #160617673]
    
    ft(upgrade):register articles app
    
    - add articles app to the settings of the app
    
    [DELIVERS #160617673]
    
    ft(upgrade):add url for articles
    
    - add url to authors project for hitting the articles app
    
    [DELIVERS #160617673]
    
    - change from createdAt to created_at, updatedAt to updated_at
    
    [DELIVERS #160617673]
    
    feat(upgrade):add generic views to create articles
    
    - modify views.py to use generic functions
    
    [DELIVERS #160617673]
    
    feat(upgrade):modify serializers to return list
    
    - import returnlist to serializers.py
    
    [DELIVERS #160617673]
    
    feat(upgrade):add view for retrieve and delete
    
    - overide the generic views functions
    - modify views to use generic views
    - modify serializers to have slug, authors field to readonly
    - add test module for articles
    
    [DELIVERS #160617673]
    
    ft(upgrade):add articles  app to authors project for creating articles
    
    - create articles app
    
    [DELIVERS #160617673]
    
    ft(upgrade):register articles app
    
    - add articles app to the settings of the app
    
    [DELIVERS #160617673]
    
    ft(upgrade):add url for articles
    
    - add url to authors project for hitting the articles app
    
    [DELIVERS #160617673]
    
    - change from createdAt to created_at, updatedAt to updated_at
    
    [DELIVERS #160617673]
    
    feat(upgrade):add generic views to create articles
    
    - modify views.py to use generic functions
    
    [DELIVERS #160617673]
    
    feat(upgrade):modify serializers to return list
    
    - import returnlist to serializers.py
    
    [DELIVERS #160617673]
    
    feat(upgrade):add view for retrieve and delete
    
    - overide the generic views functions
    
    [DELIVERS #160617673]
    
    ft(upgrade): add read time field
    
    [DELIVERS #160617684]
    
    ft(readtime): add read time field to article
    
    - add method to calculate read time in models
    
    [DELIVERS #160617684]
    
    ft(readtime): add read time feature
    
    - add function to calculate time
    - add field to returned data
    
    [DELIVERS #160617684]

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,15 +1,15 @@
 from rest_framework import serializers
 from authors.apps.articles.models import Article
 
-
 class ArticleSerializer(serializers.ModelSerializer):
     """Serializer for articles."""
     author = serializers.ReadOnlyField(source='author.username')
+    read_time=serializers.ReadOnlyField(source='read')
    
     class Meta:
         model = Article
         """ List all of the fields that could possibly be included in a request
         or response, including fields specified explicitly above."""
         
-        fields = ('author','title','slug','description','body','created_at','updated_at')
+        fields = ('author','title','slug','description','body','created_at','updated_at','read_time')
         read_only_fields = ('slug','author_id',)
--- authors/apps/articles/utils.py
+++ authors/apps/articles/utils.py
@@ -1,5 +1,7 @@
 from django.utils.text import slugify
- 
+import re
+import datetime
+import math
  
 def get_unique_slug(model_instance, slugable_field_name, slug_field_name):
     """
@@ -19,3 +21,16 @@ def get_unique_slug(model_instance, slugable_field_name, slug_field_name):
         extension += 1
  
     return unique_slug
+
+def time(string, image=0):
+        """
+        calculate the read time 
+        """
+        count = len(re.findall(r'\w+', string))
+        read_speed = 200
+        time_sec = math.ceil(count/read_speed*60)
+        readtime = int(time_sec)
+        image_time = 10
+        for image in range(image):
+            readtime += image_time
+        return str(datetime.timedelta(seconds=readtime))
